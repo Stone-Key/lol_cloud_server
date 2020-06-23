62ef31e5b96f30d0d12204cb7a175ed21f72b0f3
Stone-Key <18009095140@163.com>
2020-06-23 21:42:41 +0800

    springboot Info Project

---
+++ src/test/java/com/stone/lol_cloud/LolCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.lol_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolCloudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
